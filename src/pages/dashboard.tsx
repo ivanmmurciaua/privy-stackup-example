8c1122e60a7f7238ea4bb6ee30118c9798941149
Iván M.M <solucionesemecuadrado@gmail.com>
2023-11-16 19:33:56 +0100

    👷‍♂️ Stackup integrated and working fine

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,247 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useWallets, usePrivy } from '@privy-io/react-auth';
+import Head from 'next/head';
+import { Presets } from "userop";
+import { JsonRpcProvider } from 'ethers';
+
+export default function DashboardPage() {
+  const router = useRouter();
+  const [account, setAccount] = useState(null);
+  const {
+    ready,
+    authenticated,
+    user,
+    logout,
+    linkEmail,
+    linkWallet,
+    unlinkEmail,
+    linkPhone,
+    unlinkPhone,
+    unlinkWallet,
+    linkGoogle,
+    unlinkGoogle,
+    linkTwitter,
+    unlinkTwitter,
+    linkDiscord,
+    unlinkDiscord,
+  } = usePrivy();
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      router.push('/');
+    }
+  }, [ready, authenticated, router]);
+
+  const numAccounts = user?.linkedAccounts?.length || 0;
+  const canRemoveAccount = numAccounts > 1;
+
+  const email = user?.email;
+  const phone = user?.phone;
+  const wallet = user?.wallet;
+  const { wallets } = useWallets();
+
+  const googleSubject = user?.google?.subject || null;
+  const twitterSubject = user?.twitter?.subject || null;
+  const discordSubject = user?.discord?.subject || null;
+
+  const smartWalletGen = async () => {
+    // Wallet from Privy
+    const embeddedWallet = wallets.find((wallet: { walletClientType: string; }) => (wallet.walletClientType === 'privy'));
+    await embeddedWallet.switchChain(80001); // Change to RPC network
+
+    // Get the signer using ethers
+    const rpcUrl = process.env.NEXT_PUBLIC_STACKUP_RPC_URL;
+    const provider = await embeddedWallet.getEthersProvider();
+    const signer = provider.getSigner();
+
+    const pmUrl = process.env.NEXT_PUBLIC_STACKUP_PAYMASTER_URL;
+    
+    // Paymaster config
+    const paymasterContext = {type: "payg"};
+    const paymasterMiddleware = true 
+      ? Presets.Middleware.verifyingPaymaster(
+        pmUrl,
+        paymasterContext
+      )
+    : undefined;
+
+    // StackUp SW Generation
+    const account = await Presets.Builder.Kernel.init(
+      signer,
+      rpcUrl,
+      { paymasterMiddleware }
+    );
+
+    setAccount(account);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Privy Auth Demo</title>
+      </Head>
+
+      <main className="flex flex-col min-h-screen px-4 sm:px-20 py-6 sm:py-10 bg-privy-light-blue">
+        {ready && authenticated ? (
+          <>
+            <div className="flex flex-row justify-between">
+              <h1 className="text-2xl font-semibold">Privy Auth with StackUp Smart Wallet Generation Demo</h1>
+              <button
+                onClick={logout}
+                className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
+              >
+                Logout
+              </button>
+            </div>
+            <div className="mt-12 flex gap-4 flex-wrap">
+              {googleSubject ? (
+                <button
+                  onClick={() => {
+                    unlinkGoogle(googleSubject);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink Google
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    linkGoogle();
+                  }}
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+                >
+                  Link Google
+                </button>
+              )}
+
+              {twitterSubject ? (
+                <button
+                  onClick={() => {
+                    unlinkTwitter(twitterSubject);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink Twitter
+                </button>
+              ) : (
+                <button
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+                  onClick={() => {
+                    linkTwitter();
+                  }}
+                >
+                  Link Twitter
+                </button>
+              )}
+
+              {discordSubject ? (
+                <button
+                  onClick={() => {
+                    unlinkDiscord(discordSubject);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink Discord
+                </button>
+              ) : (
+                <button
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+                  onClick={() => {
+                    linkDiscord();
+                  }}
+                >
+                  Link Discord
+                </button>
+              )}
+
+              {email ? (
+                <button
+                  onClick={() => {
+                    unlinkEmail(email.address);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink email
+                </button>
+              ) : (
+                <button
+                  onClick={linkEmail}
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+                >
+                  Connect email
+                </button>
+              )}
+              {wallet ? (
+                <button
+                  onClick={() => {
+                    unlinkWallet(wallet.address);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink wallet
+                </button>
+              ) : (
+                <button
+                  onClick={linkWallet}
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
+                >
+                  Connect wallet
+                </button>
+              )}
+              {phone ? (
+                <button
+                  onClick={() => {
+                    unlinkPhone(phone.number);
+                  }}
+                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+                  disabled={!canRemoveAccount}
+                >
+                  Unlink phone
+                </button>
+              ) : (
+                <button
+                  onClick={linkPhone}
+                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
+                >
+                  Connect phone
+                </button>
+              )}
+            </div>
+
+            <p className="mt-6 font-bold uppercase text-sm text-gray-600">User object</p>
+            <textarea
+              value={JSON.stringify(user, null, 2)}
+              className="max-w-4xl bg-slate-700 text-slate-50 font-mono p-4 text-xs sm:text-sm rounded-md mt-2"
+              rows={20}
+              disabled
+            />
+            <br />
+            {account ? (
+              <p className="max-w-4xl flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                Your Smart Wallet: &nbsp; 
+                <code className="font-mono font-bold text-green-300">
+                  {account?.getSender()}
+                </code>
+              </p>
+              ) : (
+                <button
+                  onClick={() => {
+                    smartWalletGen();
+                  }}
+                  className="max-w-4xl text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+                >
+                  Create Smart Wallet with StackUp
+                </button>
+              )}
+          </>
+        ) : null}
+      </main>
+    </>
+  );
+}
